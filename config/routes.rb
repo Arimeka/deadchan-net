6c97b10994ca3c9a40139a47799a4a603f482539
Navi Arimeka <wrk.navi@gmail.com>
2013-11-02 09:52:27 +0400

    boards view, more styles

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
-DeadchanNet::Application.routes.draw do
+DeadchanNet::Application.routes.draw do  
   root 'home#index'
+  
+  get "boards/show"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
