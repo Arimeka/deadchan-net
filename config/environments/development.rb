0e3a42410181225635873276c568dc56f8712db3
Navi Arimeka <wrk.navi@gmail.com>
2015-02-14 16:42:59 +0300

    fix cache keys

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,7 +11,7 @@ DeadchanNet::Application.configure do
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
+  config.action_controller.perform_caching = true
   config.cache_store = :redis_store, {
     host: '127.0.0.1',
     port: 6379,
